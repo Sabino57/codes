9afa1d81d3cc538dceb6fcad9a920834aa7e0b6d
Sabino <114530733+Sabino57@users.noreply.github.com>
2023-04-16 02:55:07 -0300

    Update 6.8.py

--- 6.8.py
+++ 6.8.py
@@ -2,11 +2,11 @@
 print("calcular o peso ideal entre dois sexos")
 sexo=input("digite o sexo F/M>:").upper() #Escolher genero
 if sexo=="F" or sexo=="M":
-    altura=float(input("digite a altura:"))
+    altura=float(input("digite a altura:")) #Altura
     if altura>=0.4 or altura<=3:
         if sexo=="F" or sexo=="f": #feminino
             peso=62.1*altura-44.7
-        else: #masculino
+        else: #masculino #masculino
             peso=72.7*altura-58
         print(f"Seu peso ideal seria: {peso}")
     else: #erro
